1c531bddf92bd61c9aaa609365f093471708b28f
Sven Haardiek <Sven@Haardiek.de>
2016-12-04 14:09:53 +0100

    Start documentation...yeah

--- create.go
+++ create.go
@@ -1,5 +1,6 @@
 package set
 
+// createFromArray creates a countable Set from an arbitrary list of elements
 func createFromArray(list []interface{}) Set {
 	set := elementSet{make(map[interface{}]bool)}
 	for _, element := range list {
@@ -8,6 +9,8 @@ func createFromArray(list []interface{}) Set {
 	return set
 }
 
+// createFromFunc creates a non countable Set from an function which indicates
+// if the given element is contained in the set
 func createFromFunc(f func(interface{}) (bool, error)) Set {
 	return functionSet{f}
 }
