166a95202cb9872b9283da063b2fbe8f38440097
Sven Haardiek <Sven@Haardiek.de>
2016-12-04 10:58:23 +0100

    split function Join

--- setJoin.go
+++ setJoin.go
@@ -4,20 +4,27 @@ func Join(a Set, b Set) (Set, error) {
 
 	// if a and b countable
 	if a.Countable() && b.Countable() {
-		newSet := elementSet{}
-
-		for _, set := range []Set{a, b} {
-			list, err := set.List()
-			if err != nil {
-				return newSet, err
-			}
-			for _, element := range list {
-				newSet.elements[element] = true
-			}
-		}
-		return newSet, nil
+		return countableJoin(a, b)
 	}
+	return notCountableJoin(a, b)
+}
 
+func countableJoin(a Set, b Set) (Set, error) {
+	newSet := elementSet{make(map[interface{}]bool)}
+
+	for _, set := range []Set{a, b} {
+		list, err := set.List()
+		if err != nil {
+			return newSet, err
+		}
+		for _, element := range list {
+			newSet.elements[element] = true
+		}
+	}
+	return newSet, nil
+}
+
+func notCountableJoin(a Set, b Set) (Set, error) {
 	newContains := func(x interface{}) (bool, error) {
 		for _, set := range []Set{a, b} {
 			yes, err := set.Contains(x)
