ec5f82a8a9676a7795d7e0df98ab07304ac0e58f
Sven Haardiek <Sven@Haardiek.de>
2016-12-04 10:31:22 +0100

    Add tests

---
+++ setCreate_test.go
@@ -0,0 +1,27 @@
+package set
+
+import "testing"
+
+func TestCreateFromArray(t *testing.T) {
+	list := []interface{}{1, 2, 3, 4}
+	set := createFromArray(list)
+	if _, ok := set.(elementSet); !ok {
+		t.Error("Wrong type")
+	}
+	for _, element := range list {
+		if yes, err := set.Contains(element); err != nil {
+			t.Fatal(err)
+		} else if !yes {
+			t.Errorf("%v not in set", element)
+		}
+	}
+}
+
+func TestCreateFromFunc(t *testing.T) {
+	set := createFromFunc(func(interface{}) (bool, error) {
+		return true, nil
+	})
+	if _, ok := set.(functionSet); !ok {
+		t.Error("Wrong type")
+	}
+}
