23218fe414f197e61eb5c91a8455edd3b45a9482
Sven Haardiek <Sven@Haardiek.de>
2016-12-05 21:37:09 +0100

    fmt

--- join.go
+++ join.go
@@ -14,9 +14,9 @@ func Join(a Set, b Set) (Set, error) {
 // Creates a new set as an join of a and b. Here is assumed that a and b are
 // countable.
 func countableJoin(a Set, b Set) (Set, error) {
-  // Create a countable set
+	// Create a countable set
 	newSet := elementSet{make(map[interface{}]bool)}
-  // Loop to add all elements from a and b explicit
+	// Loop to add all elements from a and b explicit
 	for _, set := range []Set{a, b} {
 		list, err := set.List()
 		if err != nil {
@@ -33,7 +33,7 @@ func countableJoin(a Set, b Set) (Set, error) {
 // set. Therefor the resulting set is not countable.
 func notCountableJoin(a Set, b Set) (Set, error) {
 	newContains := func(x interface{}) (bool, error) {
-    // Loop to return true for all elements contains in a or b
+		// Loop to return true for all elements contains in a or b
 		for _, set := range []Set{a, b} {
 			yes, err := set.Contains(x)
 			if err != nil {
