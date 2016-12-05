a8e1ce2a4f0711cc7bf6b7801e785647317ad9db
Sven Haardiek <Sven@Haardiek.de>
2016-12-05 21:26:52 +0100

    Improve docs

--- join.go
+++ join.go
@@ -1,17 +1,22 @@
 package set
 
+// Join creates a set as the join of the sets a and b.
+//
+// If a and b are countable the resulting set is also countable otherwise the
+// resulting set not countable.
 func Join(a Set, b Set) (Set, error) {
-
-	// if a and b countable
 	if a.Countable() && b.Countable() {
 		return countableJoin(a, b)
 	}
 	return notCountableJoin(a, b)
 }
 
+// Creates a new set as an join of a and b. Here is assumed that a and b are
+// countable.
 func countableJoin(a Set, b Set) (Set, error) {
+  // Create a countable set
 	newSet := elementSet{make(map[interface{}]bool)}
-
+  // Loop to add all elements from a and b explicit
 	for _, set := range []Set{a, b} {
 		list, err := set.List()
 		if err != nil {
@@ -24,6 +29,8 @@ func countableJoin(a Set, b Set) (Set, error) {
 	return newSet, nil
 }
 
+// Creates a new set as the join of a and b by using a function to define the
+// set. Therefor the resulting set is not countable.
 func notCountableJoin(a Set, b Set) (Set, error) {
 	newContains := func(x interface{}) (bool, error) {
 		for _, set := range []Set{a, b} {
