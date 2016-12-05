50b002741a0abdcf5949e9b78aaf755383a0e446
Sven Haardiek <Sven@Haardiek.de>
2016-12-05 20:41:44 +0100

    Improve doc on create and doc

--- create.go
+++ create.go
@@ -1,9 +1,6 @@
 package set
 
-// CreateFromArray creates a countable Set from an arbitrary list of elements.
-//
-// Example:
-//	CreateFromArray([]interface{}{ true, "string", 42 })
+// Creates a countable Set from an arbitrary list of elements.
 func CreateFromArray(list []interface{}) Set {
 	set := elementSet{make(map[interface{}]bool)}
 	for _, element := range list {
@@ -12,15 +9,12 @@ func CreateFromArray(list []interface{}) Set {
 	return set
 }
 
-// CreateFromFunc creates a non countable Set from a function which indicates
+// Creates a non countable Set from a function which indicates
 // if the given element is contained in the set.
 //
 // To better support complex function it is possible to define an error which
 // is passed through the other functions like Interception to ease error
 // handling.
-//
-// Example:
-//	CreateFromFunc(func(interface{}) (bool, error)) Set { false, nil }) // empty set
 func CreateFromFunc(f func(interface{}) (bool, error)) Set {
 	return functionSet{f}
 }
--- doc.go
+++ doc.go
@@ -1,7 +1,7 @@
 /*
 Package set provides functions and an interface to work with sets of finite or
-inifite mathematical sets. There is a distiction between countable set which
-provides an explicit representation and non countable sets. which are lacking
+infinite mathematical sets. There is a distinction between countable sets which
+provides an explicit representation and non countable sets which are lacking
 some functionality and performance but are able to handle infinite sets.
 */
 package set
