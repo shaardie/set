0fb6331db9509e253c6df408f73cd0ffb04d5ee9
Sven Haardiek <sven@haardiek.de>
2017-01-16 21:27:22 +0100

    update doc

--- doc.go
+++ doc.go
@@ -1,7 +1,5 @@
 /*
-Package set provides functions and an interface to work with sets of finite or
-infinite mathematical sets. There is a distinction between countable sets which
-provides an explicit representation and non countable sets which are lacking
-some functionality and performance but are able to handle infinite sets.
+Package set provides functions and an interface to work with sets of finite or infinite mathematical sets.
+There is a distinction between definitely finite sets which provides an explicit representation and non definitely finite sets which are lacking some functionality and performance but are able to handle infinite sets.
 */
 package set
