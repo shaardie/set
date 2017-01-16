b9fe716fd2e0cd15dd06b077b329e5a306b808d8
Sven Haardiek <sven@haardiek.de>
2017-01-16 21:11:27 +0100

    Add DefinitelyFinite() to interface

--- interface.go
+++ interface.go
@@ -2,14 +2,15 @@ package set
 
 // Set implements an interface to various implementations of sets.
 //
-// Countable() implements the indicator for a set to be countable or not.
-// Countable set have the ability to count their elements and show them
-// explicit in arrays. Therefor countable set implementation should also
-// implement Cardinality() and List() since those will be used by functions
-// like Difference(a Set, b Set) to be able to speed up the handling of sets.
+// DefinitelyFinite() implements the indicator for a set to be definitely finite.
+// But caution: `DefinitlyFinite() == false` does not mean that this set could not be finite, too.
+// Definitely finite sets have the ability to count their elements and show them explicit in arrays.
+// Therefore, definitely finite set implementations should also implement Cardinality() and List() since those will be used by functions like Difference(a Set, b Set) to be able to speed up the handling of sets.
+// Countable() is deprecated. Better use DefinitelyFinite().
 type Set interface {
 	Contains(x interface{}) (bool, error)
 	Countable() bool
+	DefinitelyFinite() bool
 	Cardinality() (uint64, error)
 	List() ([]interface{}, error)
 }
